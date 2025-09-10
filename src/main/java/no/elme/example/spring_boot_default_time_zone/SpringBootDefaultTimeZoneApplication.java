82e7cc08a895d8b9e572f653b249603c313559c2
Andreas Pedro Garcia-Vedal <1391290+apgv@users.noreply.github.com>
2025-09-10 22:44:20 +0200

    Initial setup of Spring Boot project.

---
+++ src/main/java/no/elme/example/spring_boot_default_time_zone/SpringBootDefaultTimeZoneApplication.java
@@ -0,0 +1,13 @@
+package no.elme.example.spring_boot_default_time_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDefaultTimeZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDefaultTimeZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/elme/example/spring_boot_default_time_zone/SpringBootDefaultTimeZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package no.elme.example.spring_boot_default_time_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDefaultTimeZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
