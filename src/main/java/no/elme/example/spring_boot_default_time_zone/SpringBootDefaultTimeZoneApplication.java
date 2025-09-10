fd5be8759a34d373418c9d30d6816384b1bf3085
Andreas Pedro Garcia-Vedal <1391290+apgv@users.noreply.github.com>
2025-09-10 23:14:01 +0200

    Sets the default time zone to 'Antarctica/South_Pole' during startup.

--- src/main/java/no/elme/example/spring_boot_default_time_zone/SpringBootDefaultTimeZoneApplication.java
+++ src/main/java/no/elme/example/spring_boot_default_time_zone/SpringBootDefaultTimeZoneApplication.java
@@ -1,13 +1,24 @@
 package no.elme.example.spring_boot_default_time_zone;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class SpringBootDefaultTimeZoneApplication {
 
     public static void main(String[] args) {
+//        If the time zone is set here it's not picked up in Spring Boot test
+//        TimeZone.setDefault(TimeZone.getTimeZone("Antarctica/South_Pole"));
+
         SpringApplication.run(SpringBootDefaultTimeZoneApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setDefaultTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Antarctica/South_Pole"));
     }
 
 }
--- src/test/java/no/elme/example/spring_boot_default_time_zone/SpringBootDefaultTimeZoneApplicationTests.java
+++ src/test/java/no/elme/example/spring_boot_default_time_zone/SpringBootDefaultTimeZoneApplicationTests.java
@@ -3,11 +3,18 @@ package no.elme.example.spring_boot_default_time_zone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.TimeZone;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SpringBootDefaultTimeZoneApplicationTests {
 
     @Test
-    void contextLoads() {
+    void whenChangingTheDefaultTimeZoneDuringStartup_expectTimeZoneToBeTheSouthPoleAntarctica() {
+        TimeZone defaultTimeZone = TimeZone.getDefault();
+
+        assertThat(defaultTimeZone.getID()).isEqualTo("Antarctica/South_Pole");
     }
 
 }
